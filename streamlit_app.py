6d4dcb1ff65b718729a8e231f687acca459b5be6
Jin723 <90682662+Jin723@users.noreply.github.com>
2023-04-19 16:51:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title("My Helathy Diner")
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣🥑🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗ale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
 
