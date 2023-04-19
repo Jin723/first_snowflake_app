c34f5556e624ef5e58ea077ae42877e4294c7460
Jin723 <90682662+Jin723@users.noreply.github.com>
2023-04-19 16:51:53 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🥣🥑🍞Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗ale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
